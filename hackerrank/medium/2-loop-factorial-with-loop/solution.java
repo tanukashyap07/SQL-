2675381a43de7b923440f47729c922cfa46db532
Anchala <aanyakashyap838@gmail.com>
2026-08-24 10:27:39 +0530

    ✅ 2. Loop - Factorial with loop (CW) | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

---
+++ hackerrank/medium/2-loop-factorial-with-loop/solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println(fact);
+    }
+}
