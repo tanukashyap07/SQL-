f2cd83e0f4eb415945f4fb47389a6456c4a37f7b
Anchala <aanyakashyap838@gmail.com>
2026-09-24 18:59:39 +0530

    ✅ Remove Duplicates from Sorted Array | Easy | Java | Runtime: 1 ms (77.53%) | Memory: 46.4 MB | 2026-09-24

--- leetcode/easy/0026-remove-duplicates-from-sorted-array/solution.java
+++ leetcode/easy/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-        int[] arr = {1, 1, 2};
+        int k = 1;
 
-        Set<Integer> set = new TreeSet<>();
+        for (int i = 1; i < nums.length; i++) {
 
-        for (int i = 0; i < arr.length; i++) {
-            set.add(arr[i]);
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
         }
 
-        System.out.println(set);
-
-        return set.size();
+        return k;
     }
 }
